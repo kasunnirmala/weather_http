24751ff6f4ec6afbe6cebd05035eae199b314390
Kasun Nirmala <bgkasunnirmala@gmail.com>
2020-10-19 13:15:58 +0530

    server

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express();
 
 
 app.use('/weather/data', async (req, res) => {
-console.log(req.body);
+console.log(req.params);
 res.json({status:202});
 });
 
 app.listen(2121);
-
+console.log("SERVER STARTED");
